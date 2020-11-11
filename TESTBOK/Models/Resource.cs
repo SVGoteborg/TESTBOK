cad0c00fd037f9af67bdd24bd3e6ad6986452bc9
PetNil <peter.work@live.se>
2020-11-12 00:22:35 +0100

    Add ViewModell, Fluent Api and seed

--- TESTBOK/Models/Resource.cs
+++ TESTBOK/Models/Resource.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,13 +8,18 @@ namespace TESTBOK.Models
 {
     public class Resource
     {
+        
+        [Key]
         public int ResId { get; set; }
 
         // Lokal, Rum, Bil, P-plats, Mötesrum
         public string ResName { get; set; }
         public string Info { get; set; }
         public string Activity { get; set; }
-        public int Size { get; set; }
+        public int? Size { get; set; }
         public bool Bookable { get; set; }
+        public Unit Unit { get; set; }
     }
+
+    
 }
