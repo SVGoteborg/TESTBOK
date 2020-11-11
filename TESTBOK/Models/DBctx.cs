9c367412bd206676c27dfabc042a9e793ede99fa
PetNil <peter.work@live.se>
2020-11-11 16:11:59 +0100

    Small change _Layout, change DBctx comments

--- TESTBOK/Models/DBctx.cs
+++ TESTBOK/Models/DBctx.cs
@@ -13,13 +13,13 @@ namespace TESTBOK.Models
         {
         }
 
-        public DbSet<User> Users { get; set; }
+        //public DbSet<User> Users { get; set; }
         public DbSet<Unit>  Units { get; set; }
         public DbSet<Resource> Resources { get; set; }
-        public DbSet<Booking> Bookings { get; set; }
-        public DbSet<UserRole> UserRoles { get; set; }
-        public DbSet<Permission> Permissions { get; set; }
-        public DbSet<PermissionGroup> PermissionGroups { get; set; }
+        //public DbSet<Booking> Bookings { get; set; }
+        //public DbSet<UserRole> UserRoles { get; set; }
+        //public DbSet<Permission> Permissions { get; set; }
+        //public DbSet<PermissionGroup> PermissionGroups { get; set; }
 
     }
 }
