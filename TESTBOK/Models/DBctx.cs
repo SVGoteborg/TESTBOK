913b6858ba6fd2a64fb551927d94fecf27da3695
PetNil <peter.work@live.se>
2020-11-12 10:14:02 +0100

    Update DBctx relations entities and more seed.

--- TESTBOK/Models/DBctx.cs
+++ TESTBOK/Models/DBctx.cs
@@ -23,18 +23,25 @@ namespace TESTBOK.Models
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Unit>()
-                .HasMany(r => r.Resources)
-                .WithOne(u => u.Unit)
-                .HasForeignKey(r => r.ResId);
+            //modelBuilder.Entity<Unit>()
+            //    .HasMany(r => r.Resources)
+            //    .WithOne(u => u.Unit)
+            //    .HasForeignKey(r => r.ResId);
+
+            modelBuilder.Entity<Resource>()
+                .HasOne(u => u.Unit)
+                .WithMany(r => r.Resources)
+                .HasForeignKey(u => u.UnitId);
 
 
-            modelBuilder.Entity<Unit>().HasData(new Unit { UnitId = 1, UnitName = "Redbergsskolan", ShortName = "RES", Address ="Redbergsplatsen" });
-            modelBuilder.Entity<Unit>().HasData(new Unit { UnitId = 2, UnitName = "Ånässkolan", ShortName = "ÅNÄS", Address = "Ånäsplatsen" });
+            modelBuilder.Entity<Unit>().HasData(new Unit { UnitId = 1, UnitName = "Redbergsskolan", ShortName = "RES", Address = "Örngatan 6," });
+            modelBuilder.Entity<Unit>().HasData(new Unit { UnitId = 2, UnitName = "Ånässkolan", ShortName = "ÅNÄS", Address = "Falkgatan 8" });
+            modelBuilder.Entity<Unit>().HasData(new Unit { UnitId = 3, UnitName = "Redbergsteatern", ShortName = "RET", Address = "Örngatan 6," });
 
-            modelBuilder.Entity<Resource>().HasData(new Resource { ResId = 1, ResName = "Sal 11", Info = "Lektionssal", Activity = "Teorisal", Size = 15, Bookable = true });
-            modelBuilder.Entity<Resource>().HasData(new Resource { ResId = 2, ResName = "Sal 13", Info = "Lektionssal", Activity = "Teorisal", Size = 10, Bookable = true });
-            modelBuilder.Entity<Resource>().HasData(new Resource { ResId = 3, ResName = "Stor sal", Info = "Aula", Activity = "Mötessal", Size = 40, Bookable = true });
+            modelBuilder.Entity<Resource>().HasData(new Resource { ResId = 1, ResName = "Sal 11", Info = "Lektionssal", Activity = "Teorisal", Size = 15, Bookable = true, UnitId = 1 });
+            modelBuilder.Entity<Resource>().HasData(new Resource { ResId = 2, ResName = "Sal 13", Info = "Lektionssal", Activity = "Teorisal", Size = 10, Bookable = true, UnitId = 1 });
+            modelBuilder.Entity<Resource>().HasData(new Resource { ResId = 3, ResName = "Stor sal", Info = "Aula", Activity = "Mötessal", Size = 40, Bookable = true, UnitId = 2 });
+            modelBuilder.Entity<Resource>().HasData(new Resource { ResId = 4, ResName = "Teatersal", Info = "Stor sal", Activity = "Teater", Size = 40, Bookable = true, UnitId = 3 });
         }
     }
 }
